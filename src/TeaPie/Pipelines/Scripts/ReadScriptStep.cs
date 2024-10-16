b6f09db4eded1f32fbf3c217a50bf2060985a564
matejicko <matogrochal01@gmail.com>
2024-10-16 11:05:16 +0200

    draft commit

---
+++ src/TeaPie/Pipelines/Scripts/ReadScriptStep.cs
@@ -0,0 +1,13 @@
+﻿using TeaPie.Pipelines.Application;
+using TeaPie.ScriptHandling;
+
+namespace TeaPie.Pipelines.Scripts;
+internal class ReadScriptStep(ScriptExecution scriptExecution) : IPipelineStep
+{
+    private readonly ScriptExecution _scriptExecution = scriptExecution;
+
+    public async Task Execute(ApplicationContext context, CancellationToken cancellationToken = default)
+    {
+        _scriptExecution.RawContent = await File.ReadAllTextAsync(_scriptExecution.Script.File.Path, cancellationToken);
+    }
+}
