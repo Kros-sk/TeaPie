65a52aaa133db0618d696708e707a76e5b6e6f40
Matej Grochal <grochal@kros.sk>
2024-10-24 11:45:34 +0200

    addition of missing empty lines

--- src/TeaPie/Pipelines/IPipeline.cs
+++ src/TeaPie/Pipelines/IPipeline.cs
@@ -5,8 +5,12 @@ namespace TeaPie.Pipelines;
 internal interface IPipeline
 {
     Task Run(ApplicationContext context, CancellationToken cancellationToken = default);
+
     void AddSteps(params IPipelineStep[] steps);
+
     void AddSteps(params Func<ApplicationContext, CancellationToken, Task>[] lambdaFunction);
+
     void InsertSteps(IPipelineStep predecessor, params Func<ApplicationContext, CancellationToken, Task>[] lambdaFunction);
+
     void InsertSteps(IPipelineStep predecessor, params IPipelineStep[] steps);
 }
