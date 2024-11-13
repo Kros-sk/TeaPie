f570fb7783176881924710abbd16b5b27b693075
Matej Grochal <grochal@kros.sk>
2024-11-13 10:11:37 +0100

    refactor of ScriptCompilerShould

--- tests/TeaPie.Tests/ScriptHandling/ScriptCompilerShould.cs
+++ tests/TeaPie.Tests/ScriptHandling/ScriptCompilerShould.cs
@@ -3,10 +3,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using NSubstitute;
 using System.Data;
-using TeaPie.Extensions;
 using TeaPie.ScriptHandling;
-using TeaPie.StructureExploration.IO;
-using File = TeaPie.StructureExploration.IO.File;
 
 namespace TeaPie.Tests.ScriptHandling;
 
@@ -16,8 +13,8 @@ public class ScriptCompilerShould
     public async void ScriptWithSyntaxErrorShouldThrowException()
     {
         var logger = NullLogger.Instance;
-        var context = GetScriptExecutionContext(ScriptIndex.ScriptWithSyntaxErrorPath);
-        await PrepareScriptForCompilation(context);
+        var context = ScriptHelper.GetScriptExecutionContext(ScriptIndex.ScriptWithSyntaxErrorPath);
+        await ScriptHelper.PrepareScriptForCompilation(context);
 
         var compiler = new ScriptCompiler(Substitute.For<ILogger<ScriptCompiler>>());
 
@@ -27,8 +24,8 @@ public class ScriptCompilerShould
     [Fact]
     public async void PlainScriptShouldBeCompiledWithoutAnyProblem()
     {
-        var context = GetScriptExecutionContext(ScriptIndex.PlainScriptPath);
-        await PrepareScriptForCompilation(context);
+        var context = ScriptHelper.GetScriptExecutionContext(ScriptIndex.PlainScriptPath);
+        await ScriptHelper.PrepareScriptForCompilation(context);
 
         var compiler = new ScriptCompiler(Substitute.For<ILogger<ScriptCompiler>>());
 
@@ -39,45 +36,12 @@ public class ScriptCompilerShould
     [Fact]
     public async void ScriptWithNuGetPackageShouldBeCompiledWithoutAnyProblem()
     {
-        var context = GetScriptExecutionContext(ScriptIndex.ScriptWithOneNuGetDirectivePath);
-        await PrepareScriptForCompilation(context);
+        var context = ScriptHelper.GetScriptExecutionContext(ScriptIndex.ScriptWithOneNuGetDirectivePath);
+        await ScriptHelper.PrepareScriptForCompilation(context);
 
         var compiler = new ScriptCompiler(Substitute.For<ILogger<ScriptCompiler>>());
 
         var compiledScript = compiler.CompileScript(context.ProcessedContent!);
         compiledScript.Should().NotBe(null);
-    }
-
-    private static async Task PrepareScriptForCompilation(ScriptExecutionContext context)
-    {
-        context.RawContent = await System.IO.File.ReadAllTextAsync(context.Script.File.Path);
-        await PreProccessScript(context);
-    }
-
-    private static async Task PreProccessScript(ScriptExecutionContext context)
-    {
-        var nugetHandler = new NuGetPackageHandler(Substitute.For<ILogger<NuGetPackageHandler>>());
-        var processor = new ScriptPreProcessor(nugetHandler, Substitute.For<ILogger<ScriptPreProcessor>>());
-        var referencedScripts = new List<string>();
-        context.ProcessedContent = await processor.ProcessScript(
-            context.Script.File.Path,
-            context.RawContent!,
-            ScriptIndex.RootSubFolderPath,
-            Path.GetTempPath(),
-            referencedScripts);
-    }
-
-    private static ScriptExecutionContext GetScriptExecutionContext(string path)
-    {
-        var folder = new Folder(ScriptIndex.RootSubFolderPath, ScriptIndex.RootSubFolder, ScriptIndex.RootSubFolder, null);
-        var file = new File(
-            path,
-            path.TrimRootPath(ScriptIndex.RootFolderPath),
-            Path.GetFileName(ScriptIndex.ScriptAccessingTeaPieInstance),
-            folder);
-
-        var script = new Script(file);
-
-        return new ScriptExecutionContext(script);
     }
 }
