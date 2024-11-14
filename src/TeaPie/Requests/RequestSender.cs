1f03d01dd95006adfaf9bcb1a2ec2017e87e49a6
matejicko <matogrochal01@gmail.com>
2024-11-14 12:49:55 +0100

    forgotten changes

---
+++ src/TeaPie/Requests/RequestSender.cs
@@ -0,0 +1,14 @@
+﻿namespace TeaPie.Requests;
+
+internal interface IRequestSender
+{
+    Task<HttpResponseMessage> SendRequest(HttpRequestMessage message, CancellationToken cancellationToken = default);
+}
+
+internal class RequestSender(HttpClient client) : IRequestSender
+{
+    private readonly HttpClient _client = client;
+
+    public async Task<HttpResponseMessage> SendRequest(HttpRequestMessage message, CancellationToken cancellationToken = default)
+        => await _client.SendAsync(message, cancellationToken);
+}
