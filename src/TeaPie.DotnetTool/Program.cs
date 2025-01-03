674ee15608b7edb82bb477c979ab6292f8839e36
matejicko <matogrochal01@gmail.com>
2025-01-03 16:41:51 +0100

    implementation of cli tool

--- src/TeaPie.DotnetTool/Program.cs
+++ src/TeaPie.DotnetTool/Program.cs
@@ -1,16 +1,21 @@
 ﻿using Spectre.Console.Cli;
 using TeaPie.DotnetTool;
 
-var app = new CommandApp();
+var app = new CommandApp<TestCommand>();
 app.Configure(config =>
 {
-    config.SetApplicationName("tp");
+    config.SetApplicationName("teapie");
 
     config.AddCommand<TestCommand>("test")
         .WithDescription("Runs tests from the collection at the specified path. " +
         "If no path is provided, the current directory is used.")
         .WithExample("test", "[pathToCollection]")
         .WithExample("test", "\"path\\to\\collection\"");
+
+#if DEBUG
+    config.PropagateExceptions();
+    config.ValidateExamples();
+#endif
 });
 
 return await app.RunAsync(args);
