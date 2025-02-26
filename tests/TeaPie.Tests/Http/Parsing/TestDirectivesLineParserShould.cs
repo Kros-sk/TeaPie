502fc57b08a8ccf9fbf9cb315f27fa78c0378f99
Matej Grochal <grochal@kros.sk>
2025-02-26 11:10:27 +0100

    unit tests

---
+++ tests/TeaPie.Tests/Http/Parsing/TestDirectivesLineParserShould.cs
@@ -0,0 +1,85 @@
+﻿using System.Net.Http.Headers;
+using TeaPie.Http.Parsing;
+using TeaPie.Testing;
+using static Xunit.Assert;
+
+namespace TeaPie.Tests.Http.Parsing;
+
+public class TestDirectivesLineParserShould
+{
+    private readonly HttpRequestHeaders _headers = new HttpClient().DefaultRequestHeaders;
+    private readonly TestDirectivesLineParser _parser = new();
+
+    [Fact]
+    public void ParseTestExpectStatusCodesDirective()
+    {
+        const string line = "## TEST-EXPECT-STATUS: [500, 501]";
+        HttpParsingContext context = new(_headers);
+
+        True(_parser.CanParse(line, context));
+        _parser.Parse(line, context);
+
+        NotEmpty(context.Tests);
+        var testDesc = context.Tests[0];
+        Equal(PredefinedTestType.ExpectStatusCodes, testDesc.Type);
+        Equal([500, 501], (int[])testDesc.Parameters[0]);
+    }
+
+    [Fact]
+    public void ParseTestHasBodyWithArgumentDirective()
+    {
+        const string line = "## TEST-HAS-BODY: false";
+        HttpParsingContext context = new(_headers);
+
+        True(_parser.CanParse(line, context));
+        _parser.Parse(line, context);
+
+        NotEmpty(context.Tests);
+        var testDesc = context.Tests[0];
+        Equal(PredefinedTestType.HasBody, testDesc.Type);
+        False((bool)testDesc.Parameters[0]);
+    }
+
+    [Fact]
+    public void ParseTestHasBodyWithoutArgumentDirective()
+    {
+        const string line = "## TEST-HAS-BODY";
+        HttpParsingContext context = new(_headers);
+
+        True(_parser.CanParse(line, context));
+        _parser.Parse(line, context);
+
+        NotEmpty(context.Tests);
+        var testDesc = context.Tests[0];
+        Equal(PredefinedTestType.HasBody, testDesc.Type);
+        True((bool)testDesc.Parameters[0]);
+    }
+
+    [Fact]
+    public void ParseTestHasHeaderDirective()
+    {
+        const string headerName = "Content-Type";
+        var line = $"## TEST-HAS-HEADER: {headerName}";
+        HttpParsingContext context = new(_headers);
+
+        True(_parser.CanParse(line, context));
+        _parser.Parse(line, context);
+
+        NotEmpty(context.Tests);
+        var testDesc = context.Tests[0];
+        Equal(PredefinedTestType.HasHeader, testDesc.Type);
+        Equal(headerName, (string)testDesc.Parameters[0]);
+    }
+
+    [Fact]
+    public void NotParseOtherDirective()
+    {
+        const string line = "## ANOTHER-DIRECTIVE: OAuth2";
+        var context = new HttpParsingContext(_headers);
+
+        var canParse = _parser.CanParse(line, context);
+
+        False(canParse);
+        Throws<InvalidOperationException>(() => _parser.Parse(line, context));
+    }
+}
---
+++ tests/TeaPie.Tests/Testing/PredefinedTestFactoryShould.cs
@@ -0,0 +1,57 @@
+﻿using NSubstitute;
+using TeaPie.Http;
+using TeaPie.Http.Headers;
+using TeaPie.Testing;
+using static Xunit.Assert;
+
+namespace TeaPie.Tests.Testing;
+
+public class PredefinedTestFactoryShould
+{
+    private readonly PredefinedTestFactory _factory = new(Substitute.For<IHeadersHandler>());
+
+    [Fact]
+    public void ThrowExceptionForUnsupportedTestType()
+    {
+        var description = new PredefinedTestDescription((PredefinedTestType)999);
+
+        var exception = Throws<InvalidOperationException>(() => _factory.Create(description));
+        Contains("Unable to create test for unsupported test type", exception.Message);
+    }
+
+    [Fact]
+    public void CreateReturnExpectStatusCodesTest()
+    {
+        var description = new PredefinedTestDescription(PredefinedTestType.ExpectStatusCodes, new int[] { 200, 201 });
+        description.SetRequestExecutionContext(new RequestExecutionContext(null!));
+
+        var test = _factory.Create(description);
+
+        NotNull(test);
+        Contains("Status code should be one of these", test.Name);
+    }
+
+    [Fact]
+    public void CreateReturnHasBodyTest()
+    {
+        var description = new PredefinedTestDescription(PredefinedTestType.HasBody, true);
+        description.SetRequestExecutionContext(new RequestExecutionContext(null!));
+
+        var test = _factory.Create(description);
+
+        NotNull(test);
+        Contains("Response should have body", test.Name);
+    }
+
+    [Fact]
+    public void CreateReturnHasHeaderTest()
+    {
+        var description = new PredefinedTestDescription(PredefinedTestType.HasHeader, "Authorization");
+        description.SetRequestExecutionContext(new RequestExecutionContext(null!));
+
+        var test = _factory.Create(description);
+
+        NotNull(test);
+        Contains("Response should have header with name 'Authorization'", test.Name);
+    }
+}
