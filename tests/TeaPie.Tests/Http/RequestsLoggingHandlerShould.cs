c8efe2f7275bc64ac15bb0f8276cef2e8d6f4a81
Kantorová Michaela <kantorova@kros.sk>
2025-12-05 13:50:24 +0100

    added simple unit tests

---
+++ tests/TeaPie.Tests/Http/RequestsLoggingHandlerShould.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.Extensions.Logging;
+using NSubstitute;
+using System.Net;
+using TeaPie.Http;
+using TeaPie.Http.Auth;
+using TeaPie.Logging;
+using TeaPie.StructureExploration;
+using static Xunit.Assert;
+
+namespace TeaPie.Tests.Http;
+
+public class RequestsLoggingHandlerShould
+{
+    private static readonly HttpRequestOptionsKey<RequestExecutionContext> _contextKey = new("__TeaPie_Context__");
+
+    [Theory]
+    [InlineData(false)]
+    [InlineData(true)]
+    public async Task LogRequestLogEntry(bool shouldFail)
+    {
+        var (handler, logger, innerHandler) = CreateHandler();
+        var request = CreateRequest();
+        var invoker = new HttpMessageInvoker(handler);
+
+        if (shouldFail)
+        {
+            innerHandler.Exception = new HttpRequestException("Network error");
+            await ThrowsAsync<HttpRequestException>(
+                async () => await invoker.SendAsync(request, CancellationToken.None));
+        }
+        else
+        {
+            await invoker.SendAsync(request, CancellationToken.None);
+        }
+
+        logger.Received(1).Log(
+            LogLevel.Information,
+            Arg.Any<EventId>(),
+            Arg.Is<object>(state =>
+                state.ToString()!.Equals(typeof(RequestLogFileEntry).FullName)),
+            null,
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [Fact]
+    public async Task NotLogWhenContextIsMissing()
+    {
+        var (handler, logger, _) = CreateHandler();
+        var request = new HttpRequestMessage(HttpMethod.Get, "https://example.com");
+
+        var invoker = new HttpMessageInvoker(handler);
+        await invoker.SendAsync(request, CancellationToken.None);
+
+        logger.DidNotReceive().Log(
+            Arg.Any<LogLevel>(),
+            Arg.Any<EventId>(),
+            Arg.Any<object>(),
+            Arg.Any<Exception>(),
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    [Fact]
+    public async Task LogMultipleConsecutiveRequests()
+    {
+        var (handler, logger, _) = CreateHandler();
+        var invoker = new HttpMessageInvoker(handler);
+
+        await invoker.SendAsync(CreateRequest(), CancellationToken.None);
+        await invoker.SendAsync(CreateRequest(), CancellationToken.None);
+        await invoker.SendAsync(CreateRequest(), CancellationToken.None);
+
+        logger.Received(3).Log(
+            LogLevel.Information,
+            Arg.Any<EventId>(),
+            Arg.Is<object>(state =>
+                state.ToString()!.Equals(typeof(RequestLogFileEntry).FullName)),
+            null,
+            Arg.Any<Func<object, Exception?, string>>());
+    }
+
+    private static (RequestsLoggingHandler handler, ILogger logger, TestHttpMessageHandler innerHandler) CreateHandler()
+    {
+        var authAccessor = Substitute.For<IAuthProviderAccessor>();
+        var loggerFactory = Substitute.For<ILoggerFactory>();
+        var logger = Substitute.For<ILogger>();
+        loggerFactory.CreateLogger(Arg.Any<string>()).Returns(logger);
+
+        var handler = new RequestsLoggingHandler(authAccessor, loggerFactory);
+        var innerHandler = new TestHttpMessageHandler();
+        handler.InnerHandler = innerHandler;
+
+        return (handler, logger, innerHandler);
+    }
+
+    private static HttpRequestMessage CreateRequest()
+    {
+        var file = new InternalFile("test.http", "test.http", new Folder("", "", "", null));
+        var context = new RequestExecutionContext(file);
+
+        var request = new HttpRequestMessage(HttpMethod.Get, "https://example.com");
+        request.Options.Set(_contextKey, context);
+
+        return request;
+    }
+
+    private class TestHttpMessageHandler : HttpMessageHandler
+    {
+        public HttpResponseMessage? Response { get; set; }
+        public Exception? Exception { get; set; }
+
+        protected override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            if (Exception != null)
+            {
+                throw Exception;
+            }
+            return Task.FromResult(Response ?? new HttpResponseMessage(HttpStatusCode.OK));
+        }
+    }
+}
